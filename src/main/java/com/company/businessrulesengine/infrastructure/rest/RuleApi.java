1afcf5146f48c6528cc927f4494b6a3c52e5f292
Jaquelini Rocha <jaquelini.rocha@hotmail.com>
2020-12-04 16:40:04 -0300

    adicionado busca de todas as regras cadastradas

--- src/main/java/com/company/businessrulesengine/infrastructure/rest/RuleApi.java
+++ src/main/java/com/company/businessrulesengine/infrastructure/rest/RuleApi.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collection;
+
 @RestController
 @RequestMapping("/rule")
 public class RuleApi {
@@ -28,6 +30,11 @@ public class RuleApi {
 	@GetMapping("/{id}")
 	public ResponseEntity<Rule> findById(@PathVariable Long id) {
 		return ResponseEntity.ok(ruleService.findById(id));
+	}
+
+	@GetMapping
+	public ResponseEntity<Collection<Rule>> findById() {
+		return ResponseEntity.ok(ruleService.findAll());
 	}
 
 }
